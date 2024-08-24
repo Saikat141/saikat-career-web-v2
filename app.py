f95234e32c1f4da21f7912bc095ca29071372619
Saikat141 <72371927+Saikat141@users.noreply.github.com>
2024-08-24 10:01:33 +0000

    Added flask dev

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Welcome! This is the home page."
+
+if __name__ == "__main__":
+    app.run("0.0.0.0",debug=True)
